989c703dbf32481dc599da1db4648567b201ae1b
acthtml <acthtml@gmail.com>
2013-05-14 07:32:42 +0800

    Theme start.

---
+++ sites/all/themes/pushsay/templates/html.tpl.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+
+<body class="<?php print $classes; ?>">
+  <div id="skip-link">
+  	<a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
---
+++ sites/all/themes/pushsay/templates/page.tpl.php
@@ -0,0 +1,59 @@
+<div id="page">
+  <header id="header">
+    <section class="container">
+      <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <?php if ($site_name || $site_slogan): ?>
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="logo"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+              </strong></div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="logo">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+              </h1>
+            <?php endif; ?>
+          <?php endif; ?>
+      <?php endif; ?>
+      <div class="nav-collapse collapse">
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('nav')), 'heading' => t('Main menu'))); ?>
+      </div><!--/.nav-collapse -->
+
+      <?php print render($page['header']); ?>
+
+    </section>
+  </header>
+  <div id="main">
+    <section class="container">
+      <div id="content">
+        <?php if ($breadcrumb): ?>
+          <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+        <?php endif; ?>
+        <?php print $messages; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div>
+      <?php if ($page['sidebar']): ?>
+        <div id="sidebar" class="column sidebar">
+          <?php print render($page['sidebar']); ?>
+        </div>
+      <?php endif; ?>
+    </section>
+  </div>
+  <div id="footer">
+    <section class="container">
+      <?php print render($page['footer']); ?>
+    </section>
+  </div>
+</div>
