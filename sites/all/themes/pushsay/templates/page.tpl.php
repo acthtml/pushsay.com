9a27267a2bc2ae0ea6e2eacd167ac7b7b5de4c3a
achtml <acthtml@gmail.com>
2013-05-14 17:41:11 +0800

    Content added.

--- sites/all/themes/pushsay/templates/page.tpl.php
+++ sites/all/themes/pushsay/templates/page.tpl.php
@@ -1,6 +1,6 @@
 <div id="page">
-  <header id="header">
-    <section class="container">
+  <div id="header">
+    <div class="container">
       <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -20,24 +20,21 @@
           <?php endif; ?>
       <?php endif; ?>
       <div class="nav-collapse collapse">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('nav')), 'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('nav')))); ?>
       </div><!--/.nav-collapse -->
 
       <?php print render($page['header']); ?>
 
-    </section>
-  </header>
+      <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+    </div>
+  </div>
   <div id="main">
-    <section class="container">
+    <div class="container">
       <div id="content">
-        <?php if ($breadcrumb): ?>
-          <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-        <?php endif; ?>
         <?php print $messages; ?>
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
@@ -49,11 +46,11 @@
           <?php print render($page['sidebar']); ?>
         </div>
       <?php endif; ?>
-    </section>
+    </div>
   </div>
   <div id="footer">
-    <section class="container">
+    <div class="container">
       <?php print render($page['footer']); ?>
-    </section>
+    </div>
   </div>
 </div>
