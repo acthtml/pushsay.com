e9bb1d3a6cc6248828fb42899291b153e058bd5c
acthtml <acthtml@gmail.com>
2013-05-16 07:28:09 +0800

    Theme end patch.

--- sites/all/themes/pushsay/templates/html.tpl.php
+++ sites/all/themes/pushsay/templates/html.tpl.php
@@ -3,6 +3,7 @@
 <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
